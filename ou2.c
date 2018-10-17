d88d18b9f2287e127a0523257ad95ce53d4c61b6
wamidol <43821223+wamidol@users.noreply.github.com>
2018-10-17 10:23:29 +0200

    Create ou2.c

---
+++ ou2.c
@@ -0,0 +1,39 @@
+/*
+ * Programmeringsteknik med C och Matlab
+ * Fall 2018
+ * Assignment 1
+
+ * File:         ou1.c
+ * Description:  A simple implementation of a shopping assistant with a
+ *               currency converter.
+ * Author:       Mikolaj Wagner
+ * CS username:  c18mwj
+ * Date:         10/10
+ * Input:        Price of a unspecific number of items and the choice of a
+ *               specific exchange rate.
+ * Output:       Prints the menu, prints the sum of the diffrent items bought,
+ *               and prints the sum in Swedish currency and the chosen currency.
+ * Limitations:  No validation of input.
+ 1. Skriv ut information 
+ 2. Läs in antal domare 
+ 3. Läs in domarpoäng 
+ 4. Skriv ut de inlästa domarpoängen 
+ 5. Räkna fram resultat 6. Skriv ut resultat 
+
+Program information 
+The program reads in the number of judges and the score from each judge. 
+Then it calculates the average score without regard to the lowest and highest judge score. 
+Finally it prints the results (the highest, the  lowest and the final average score). 
+
+Number of judges (min 3 and max 10 judges)? 
+ 
+Score from judge 1-oädndligt?  
+Loaded scores: 
+Judge 1:
+Jugde 2: osv
+
+Final result: 
+Highest judge score:  
+Lowest judge score: 
+Final average score: 
+*/
