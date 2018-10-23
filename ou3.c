dc00855eaab12ca1615a406bc098e40d656d95f7
wamidol <43821223+wamidol@users.noreply.github.com>
2018-10-23 11:58:32 +0200

    Create ou3.c

---
+++ ou3.c
@@ -0,0 +1,155 @@
+/*
+ * Programmeringsteknik med C och Matlab
+ * Fall 18
+ * Assignment 3
+
+ * File:         ou3.c
+ * Description:  A simple implementation of Conway's Game of Life.
+ * Author:       Mikolaj Wagner   
+ * CS username:  c18mwj   
+ * Date:         25/10   
+ * Input:        Choice of initial configuration and then instruction to step
+ *               or exit.
+ * Output:       Prints the game field in each step.
+ * Limitations:  No validation of input.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Constants, representation of states */
+#define ALIVE 'X'
+#define DEAD '.'
+
+/* Declaration of data structure */
+typedef struct{
+  char current;
+  char next;
+} cell;
+
+/* Declaration of functions */
+void initField(const int rows, const int cols, cell field[rows][cols]);
+void loadGlider(const int rows, const int cols, cell field[rows][cols]);
+void loadSemaphore(const int rows, const int cols, cell field[rows][cols]);
+void loadRandom(const int rows, const int cols, cell field[rows][cols]);
+void loadCustom(const int rows, const int cols, cell field[rows][cols]);
+
+
+/* Function:    main
+ * Description: Start and run games, interact with the user.
+ * Input:       About what initial structure and whether to step or exit.
+ * Output:      Information to the user, and the game field in each step.
+ */
+
+int main(void) {
+
+    return 0;
+}
+
+
+/* Function:    initField
+ * Description: Initialize all the cells to dead, then asks the user about
+ *              which structure to load, and finally load the structure.
+ * Input:       The field array and its size.
+ * Output:      The field array is updated.
+ */
+
+void initField(const int rows, const int cols, cell field[rows][cols]) {
+
+    for (int r = 0 ; r < rows ; r++) {
+        for (int c = 0 ; c < cols ; c++) {
+            field[r][c].current = DEAD;
+        }
+    }
+
+    printf("Select field spec to load ([G]lider, [S]emaphore, [R]andom ");
+    printf("or [C]ustom): ");
+
+    int ch = getchar();
+    
+	/* Ignore following newline */
+    if (ch != '\n') {
+        getchar();
+    }
+
+    switch (ch) {
+        case 'g':
+        case 'G':
+            loadGlider(rows, cols, field);
+            break;
+        case 's':
+        case 'S':
+            loadSemaphore(rows, cols, field);
+            break;
+        case 'r':
+        case 'R':
+            loadRandom(rows, cols, field);
+            break;
+        case 'c':
+        case 'C':
+        default:
+            loadCustom(rows, cols, field);
+            break;
+    }
+}
+
+
+/* Function:    loadGlider
+ * Description: Inserts a glider into the field.
+ * Input:       The field array and its size.
+ * Output:      The field array is updated.
+ */
+
+void loadGlider(const int rows, const int cols, cell field[rows][cols]) {
+
+    field[0][1].current = ALIVE;
+    field[1][2].current = ALIVE;
+    field[2][0].current = ALIVE;
+    field[2][1].current = ALIVE;
+    field[2][2].current = ALIVE;
+}
+
+
+/* Function:    loadSemaphore
+ * Description: Inserts a semaphore into the field.
+ * Input:       The field array and its size.
+ * Output:      The field array is updated.
+ */
+
+void loadSemaphore(const int rows, const int cols, cell field[rows][cols]) {
+
+    field[8][1].current = ALIVE;
+    field[8][2].current = ALIVE;
+    field[8][3].current = ALIVE;
+}
+
+
+/* Function:    loadRandom
+ * Description: Inserts a random structure into the field.
+ * Input:       The field array and its size.
+ * Output:      The field array is updated. There is a 50 % chance that a cell
+ *              is alive.
+ */
+
+void loadRandom(const int rows, const int cols, cell field[rows][cols]) {
+
+}
+
+
+/* Function:    loadCustom
+ * Description: Lets the user specify a structure that then is inserted into
+ *              the field.
+ * Input:       The field array and its size.
+ * Output:      The field array is updated.
+ */
+
+void loadCustom(const int rows, const int cols, cell field[rows][cols]) {
+
+    printf("Give custom format string: ");
+    do {
+        int r, c;
+        scanf("%d,%d", &r, &c);
+        field[r][c].current = ALIVE;
+    } while (getchar() != '\n');
+}
